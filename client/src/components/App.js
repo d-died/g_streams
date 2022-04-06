54618567bd57caf55b4c0a394376998ef2b3194a
dsump <dsumpmann@gmail.com>
2022-04-06 11:54:28 -0600

    All links established

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
     return (
-        <div>
-            <Router>
-               
+        <div className="ui container">
+            <Router>    
+                <div>
+                    <Header />
+                   <Route path='/' exact component={StreamList}/>
+                   <Route path='/streams/new' exact component={StreamCreate}/>
+                   <Route path='/streams/edit' exact component={StreamEdit}/>
+                   <Route path='/streams/delete' exact component={StreamDelete}/>
+                   <Route path='/streams/show' exact component={StreamShow}/>
+                </div>
             </Router>
-            App Hi!
         </div>
     )
 }
