174b941520082d2ebe35c473ade61f0bf8251373
dsump <dsumpmann@gmail.com>
2022-04-07 13:52:32 -0600

    Adding in redux form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Field, reduxForm } from 'redux-form'
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
+class StreamCreate extends React.Component {
+
+    renderInput({ input }) {
+        return (
+            // this takes all keyvalue properties and add them to the form object
+            <input { ...input }/>
+        )
+    }
+
+    render() {
+        console.log(this.props)
+        return (
+            <form>
+                <Field name='title' component={ this.renderInput }/>
+                <Field name='description' component={ this.renderInput }/>
+            </form>
+        )
+    }
 }
 
-export default StreamCreate
+// this is just redux form storing all of the information from the form
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate)
